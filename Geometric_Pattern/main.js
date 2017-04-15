a677772d00012291567b6cc0ecd4af4464964c59
Caleb Shield <vortexart7@gmail.com>
2017-04-15 22:49:23 +0000

    Create Dodge game

--- Geometric_Pattern/main.js
+++ Geometric_Pattern/main.js
@@ -1,6 +1,9 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
 var circleRadius;
+var rVal; 
+var gVal; 
+var bVal; 
 
 function setup() {
   createCanvas(480, 600);
@@ -9,10 +12,14 @@ function setup() {
 }
 
 function draw() {
+    rVal = 255;
+  gVal = 0;
+  bVal = 0;
+
     var isShifted = false;
     
-  var y = 0;
-  while (y <= height) {
+  var y = height;
+  while (y >= 0) {
 
     var x;
     
@@ -22,11 +29,13 @@ function draw() {
          x = 0;
      }
     while (x <= width) {
+        fill(color(255, 0, 0));
+        stroke(color(0, 255, 0));
       ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
     }
 
-    y = y + circleRadius;
+    y = y - circleRadius;
     isShifted = !isShifted;
   }
 }
